fe928f2584b3bfb23ca8d7c799032f4d4bf79a9c
Robert Fischer <robert@programming-with-design.at>
2018-07-02 23:35:46 +0200

    correct orientation

--- src/core/preprocess_image.py
+++ src/core/preprocess_image.py
@@ -6,7 +6,7 @@ from PIL import Image
 from core.utility import show_img_and_wait
 
 
-def preprocess_image(path, show_img=False):
+def preprocess_image(path):
     """
 
     Preprocesses the image given by path. Extracts the cartoon and a OCR version of the punchline
@@ -37,13 +37,55 @@ def preprocess_image(path, show_img=False):
             max_contour = c
     assert max_contour is not None
 
+    # cartoon should be on top => rotate if not
+    x, y, w, h = cv2.boundingRect(max_contour)
+    top = y
+    left = x
+    right = original_img_width - (x + w)
+    bottom = original_img_width - (y + h)
+
+    if right > top and right > left and right > bottom:
+        # the text is on the right => +90°
+        blurred = cv2.transpose(blurred)
+        blurred = cv2.flip(blurred, flipCode=1)
+
+    if left > top and left > right and left > bottom:
+        # the text is on the left => -90°
+        blurred = cv2.transpose(blurred)
+        blurred = cv2.flip(blurred, flipCode=0)
+
+    if top > bottom and top > left and top > right:
+        # the text is on the bottom => +189°
+        blurred = cv2.transpose(blurred)
+        blurred = cv2.flip(blurred, flipCode=0)
+        blurred = cv2.transpose(blurred)
+        blurred = cv2.flip(blurred, flipCode=0)
+
+    original_img_height, original_img_width = blurred.shape
+
+    edges = cv2.Canny(blurred, 100, 200, 3)
+    im2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    max_area = 0
+    max_contour = None
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        x, y, w, h = cv2.boundingRect(c)
+        if area > max_area and abs(w * h - area) < area * 0.1:
+            max_area = area
+            max_contour = c
+    assert max_contour is not None
+
+    # cartoon should be on top => rotate if not
+    x, y, w, h = cv2.boundingRect(max_contour)
+
     # cut out cartoon
-    mask = np.zeros_like(original_img)  # create empty mask
+    mask = np.zeros_like(blurred)  # create empty mask
     cv2.drawContours(mask, [max_contour], 0, 255, -1)  # draw filled contour into mask
     kernel = np.ones((16, 16), np.uint8)
     mask = cv2.erode(mask, kernel, iterations=1)  # remove borders from mask
-    out = np.zeros_like(original_img)
-    out[mask == 255] = original_img[mask == 255]  # copy images over where mask is 255
+    out = np.zeros_like(blurred)
+    out[mask == 255] = blurred[mask == 255]  # copy images over where mask is 255
     (x, y) = np.where(mask == 255)  # crop out image
     (topx, topy) = (np.min(x) + 1, np.min(y) + 1)
     (bottomx, bottomy) = (np.max(x) - 1, np.max(y) - 1)
@@ -51,7 +93,7 @@ def preprocess_image(path, show_img=False):
 
     # extract text => below cartoon
     x, y, w, h = cv2.boundingRect(max_contour)
-    punchline = original_img[y + h:y + h + (original_img_height - (y + h)), 0:original_img_width]
+    punchline = blurred[y + h:y + h + (original_img_height - (y + h)), 0:original_img_width]
     blurred = cv2.GaussianBlur(punchline, (3, 3), 0)
     edges = cv2.Canny(blurred, 100, 200, 3)
     im2, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -69,21 +111,13 @@ def preprocess_image(path, show_img=False):
 
     # apply tesseract
     punchline = punchline[min_y:max_y, min_x:max_x]
-    #punchline = cv2.GaussianBlur(punchline, (1, 1), 0)
     punchline = cv2.resize(punchline, None, fx=8, fy=8)
     punchline = 255 - punchline  # invert color, so we can use the threshold function
     _, punchline = cv2.threshold(punchline, 45, 255, cv2.THRESH_TOZERO)  # remove background noise
     punchline = cv2.equalizeHist(punchline)  # normalize brightness
     _, punchline = cv2.threshold(punchline, 140, 255, cv2.THRESH_TOZERO)  # now make text "thinner"
     punchline = 255 - punchline  # back to normal
-    #punchline = cv2.medianBlur(punchline, 3)
-    #punchline = cv2.adaptiveThreshold(punchline, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    #_, punchline = cv2.threshold(punchline, 0, 255, cv2.THRESH_OTSU)
+    _, punchline = cv2.threshold(punchline, 0, 255, cv2.THRESH_OTSU)
     punchline_text = pytesseract.image_to_string(Image.fromarray(punchline), lang='eng')
-    print("Extracted Punchline: " + punchline_text)
-
-    if show_img:
-        show_img_and_wait(out)
-        show_img_and_wait(punchline)
 
     return out, punchline_text
--- src/tests/preprocess_image_tests.py
+++ src/tests/preprocess_image_tests.py
@@ -8,7 +8,7 @@ def test_sampleimage1():
     path = './resources/sample_comic.jpg'
 
     # act
-    cartoon, punchline = preprocess_image(path=path, show_img=True)
+    cartoon, punchline = preprocess_image(path=path)
 
     # assert
     assert_that(punchline, is_('When dumb animals attempt murder.'))
@@ -19,7 +19,18 @@ def test_sampleimage2():
     path = './resources/sample_comic2.jpg'
 
     # act
-    cartoon, punchline = preprocess_image(path=path, show_img=True)
+    cartoon, punchline = preprocess_image(path=path)
 
     # assert
-    assert_that(punchline, is_('"Something\'s wrong here, Harriet...This is starting to\nlook less and less like Interstate 95."'))
+    # Something\'s wrong here, Harriet...This is starting to\nlook less and less like Interstate 95.
+    assert_that(punchline, is_('”Something’s wrong here. Hamet . . . This Is stoning to\nlook less and less like Interstate 95."'))
+
+def test_wrong_orientation():
+    # arrange
+    path = './resources/wrong_orientation.jpg'
+
+    # act
+    cartoon, punchline = preprocess_image(path=path)
+
+    # assert
+    assert_that(punchline, is_('"asdf"'))
