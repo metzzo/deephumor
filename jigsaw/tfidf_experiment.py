3a6108c74f1969257db0844d0794872c3b92da12
metzzo <robert@programming-with-design.at>
2019-06-03 09:11:59 +0200

    best result, using AutoML  0.26943699731903487

--- jigsaw/tfidf_experiment.py
+++ jigsaw/tfidf_experiment.py
@@ -2,6 +2,7 @@ import pickle
 import pandas as pd
 import numpy as np
 
+from hpsklearn import HyperoptEstimator, svc
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
@@ -16,24 +17,20 @@ def main():
 
     vectorizer = TfidfVectorizer()
     train_punchline = vectorizer.fit_transform(train_df['punchline']).toarray()
-    train_category = np.array(train_df[['funniness']])
+    train_category = np.array(train_df[['funniness']]).flatten()
 
     validation_punchline = vectorizer.transform(validation_df['punchline']).toarray()
-    validation_category = np.array(validation_df[['funniness']])
+    validation_category = np.array(validation_df[['funniness']]).flatten()
 
-    clf = SVC(
-        C=1.0,
-        kernel='rbf',
-        verbose=True
-    )
+    clf = HyperoptEstimator(classifier=svc('mySVC'))
     clf.fit(X=train_punchline, y=train_category)
+    print(clf.score(validation_punchline, validation_category))
+
     pred = clf.predict(validation_punchline)
     print("", accuracy_score(
         y_true=validation_category,
         y_pred=pred
     ))
-
-
 
 
 if __name__ == '__main__':
